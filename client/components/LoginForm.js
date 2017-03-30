c98640ef525cc0adc2acd4a696ad8c06c6bf2b9b
ankhangkieu <ankhangkieu@gmail.com>
2017-03-29 23:31:24 -0700

    Front End

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -6,6 +6,8 @@
 
 import React from 'react';
 
+import '../stylesheets/containers/LoginForm.scss';
+
 class LoginForm extends React.Component {
 
     constructor(props) {
@@ -13,9 +15,17 @@ class LoginForm extends React.Component {
 
         // binding submitFormFunction to this. this refers to our current class.
         this.submitFormFunction = this.submitFormFunction.bind(this);
+        this.onSubmitEvent = this.onSubmitEvent.bind(this);
+        this.onFacebookLoginEvent = this.onFacebookLoginEvent.bind(this);
+        this.onGoogleLoginEvent = this.onGoogleLoginEvent.bind(this);
+        this.onEmailInput = this.onEmailInput.bind(this);
+        this.onPasswordInput = this.onPasswordInput.bind(this);
+        this.onForgotPassword = this.onForgotPassword.bind(this);
 
         this.state = {
-
+            email: "None",
+            password: "None",
+            space: "  "
         }
     }
     
@@ -28,7 +38,41 @@ class LoginForm extends React.Component {
     submitFormFunction(event) {
         // Javascript regular function definition, same function as above, but DOESN'T autobind to this. You need to bind it in constructor.
         // Run this when the form gets submitted, prevent the default action
+       
         event.preventDefault();
+        // var data = {
+        //     username : this.state.email
+        //     password : this.state.password
+        // };
+        // this.props.loginFunction(data);
+    }
+
+    onFacebookLoginEvent(event) {
+        
+    }
+
+    onGoogleLoginEvent(event) {
+        
+    }
+
+    onSubmitEvent(event) {
+        this.props.submitFormFunction(event);
+    }
+
+    onEmailInput(event) {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    onPasswordInput(event) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    onForgotPassword(event) {
+
     }
 
     componentDidMount() {
@@ -39,7 +83,39 @@ class LoginForm extends React.Component {
     render() {
         // This is where you place your HTML. Inside of here goes components and other HTML elements
         return (
-            <form onSubmit={this.submitForm}>
+            <form className = "loginPage" onSubmit={this.submitForm}>
+                <div>
+                    <button className = "facebookLogin" id="FacebookLogin" type="submit" value="Submit" onClick={this.onFacebookLoginEvent}>Login with Facebook</button>
+                    <button className = "googleLogin" id="GoogleLogin" type="submit" value="Submit" onClick={this.onGoogleLoginEvent}>Login with Google</button>
+                </div>
+                <div>
+                    <span className="or">or</span>
+                    <hr className="hr"/>
+                </div>
+                <div> 
+                    <input onChange={this.onEmailInput}
+                        required
+                        className="emailBox"
+                        placeholder = "Email Address"
+                        ref = "email"
+                        type = "email"
+                    />
+                    <input onChange={this.onPasswordInput}
+                        required
+                        className="passwordBox"
+                        placeholder = "Password"
+                        ref = "password"
+                        type = "password"
+                    />
+                </div>
+                <div>
+                    <label for="rememberMe2" className = "rememberMe">                    
+                        <input id = "rememberMe2" type = "checkbox" ref = "Remember me" placeholder = "Remember me"/>
+                        {this.state.space}Remember Me
+                    </label>
+                    <a href="./forgotPassword" className="forgotPassword">Forgot Password?</a>
+                </div>
+                <button className="loginButton" id="Submit" type="submit" value="Submit" onClick={this.onSubmitEvent}>Login</button>
             </form>
         );
     }
